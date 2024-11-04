9e6e35ac733fcc4cc5d2bf070c1d51c3f96e44f7
GH818 <63499259+gaohan-cmd@users.noreply.github.com>
2024-11-04 20:48:24 +0800

    Domain Renewal

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ passwd = os.environ.get('PASSWD')
 # server酱
 SCKEY = os.environ.get('SCKEY')
 
-login_url = 'https://ikuuu.pw/auth/login'
-check_url = 'https://ikuuu.pw/user/checkin'
-info_url = 'https://ikuuu.pw/user/profile'
+login_url = 'https://ikuuu.one/auth/login'
+check_url = 'https://ikuuu.one/user/checkin'
+info_url = 'https://ikuuu.one/user/profile'
 
 header = {
-        'origin': 'https://ikuuu.pw',
+        'origin': 'https://ikuuu.one',
         'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
 }
 data = {
